01e17222787a9bca11291be757f0b5f03b39f9d7
Riccardo Bellini <riccardo.bellini@rold.com>
2024-12-20 17:33:46 +0100

    Solve chapter 3 exercises

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,77 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+
+    fn check_product(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name cannot be longer than 300 bytes");
+        }
+    }
+
+    fn check_quantity(quantity: u32) {
+        if quantity == 0 {
+            panic!("Quantity must be greater than 0");
+        }
+    }
+
+    fn check_price(unit_price: u32) {
+        if unit_price == 0 {
+            panic!("Unit price must be greater than 0");
+        }
+    }
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::check_product(&product_name);
+        Self::check_quantity(quantity);
+        Self::check_price(unit_price);
+        
+        Self {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        &self.unit_price * &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn set_unit_price(&mut self, price: u32) {
+        Self::check_price(price);
+        self.unit_price = price;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Self::check_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Self::check_product(&product_name);
+        self.product_name = product_name;
+    }
+}
