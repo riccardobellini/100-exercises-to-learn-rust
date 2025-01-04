c58b50ef1d319e46c554f086eb5f7ed7939c9ab3
Riccardo Bellini <riccardo.bellini1988@gmail.com>
2025-01-04 03:57:54 +0100

    Solve chapter 5 exercises

--- exercises/05_ticket_v2/15_outro/src/description.rs
+++ exercises/05_ticket_v2/15_outro/src/description.rs
@@ -2,7 +2,47 @@
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
 
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq,   Clone)]
 pub struct TicketDescription(String);
+
+#[derive(Debug)]
+pub struct TicketDescriptionError(String);
+
+impl Display for TicketDescriptionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl TryFrom<String> for TicketDescription {
+    type Error = TicketDescriptionError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(TicketDescriptionError("The description cannot be empty".into()))
+        }
+        if value.len() > 500 {
+            return Err(TicketDescriptionError("The description cannot be longer than 500 bytes".into()))
+        }
+        Ok(TicketDescription(value))
+    }
+}
+
+impl TryFrom<&str> for TicketDescription {
+    type Error = TicketDescriptionError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(TicketDescriptionError("The description cannot be empty".into()))
+        }
+        if value.len() > 500 {
+            return Err(TicketDescriptionError("The description cannot be longer than 500 bytes".into()))
+        }
+        Ok(TicketDescription(String::from(value)))
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/05_ticket_v2/15_outro/src/title.rs
+++ exercises/05_ticket_v2/15_outro/src/title.rs
@@ -2,7 +2,34 @@
 //   enforcing that the title is not empty and is not longer than 50 bytes.
 //   Implement the traits required to make the tests pass too.
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct TicketTitle(String);
+
+impl TryFrom<String> for TicketTitle {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(format!("The title cannot be empty"))
+        }
+        if value.len() > 50 {
+            return Err(format!("The title cannot be longer than 50 bytes"))
+        }
+        Ok(Self(value))
+    }
+}
+
+impl TryFrom<&str> for TicketTitle {
+    type Error = String;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.is_empty() {
+            return Err(format!("The title cannot be empty"))
+        }
+        if value.len() > 50 {
+            return Err(format!("The title cannot be longer than 50 bytes"))
+        }
+        Ok(Self(String::from(value)))
+    }
+}
 
 #[cfg(test)]
 mod tests {
